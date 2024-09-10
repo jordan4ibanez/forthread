6ca77f77a764fef4257c0fd2a186513e1d1734b3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-10 05:33:38 -0400

    This is actually horrible for performance

--- src/thread_filo_queue.f90
+++ src/thread_filo_queue.f90
@@ -70,19 +70,6 @@ module thread_filo_queue
   end interface concurrent_linked_filo_queue
 
 
-  interface
-
-    subroutine malloc_trim(i) bind(c, name = "malloc_trim")
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      integer(c_int), intent(in), value :: i
-    end subroutine malloc_trim
-
-
-  end interface
-
-
 contains
 
 
@@ -195,8 +182,6 @@ contains
     if (.not. associated(this%head)) then
       this%tail => null()
     end if
-
-    call malloc_trim(0)
 
     !! END SAFE OPERATION.
     status = thread_unlock_lock(this%c_mutex_pointer)
