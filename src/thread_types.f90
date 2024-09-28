3b713f6ffd4225b7acec7950b2900fbe3d35d32a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-28 18:36:11 -0400

    document this

--- src/thread_types.f90
+++ src/thread_types.f90
@@ -3,22 +3,20 @@ module thread_types
   implicit none
 
 
-  ! A raw thread queue element.
+  !* A raw thread queue element.
   type :: thread_queue_element
     type(c_funptr) :: function_ptr = c_null_funptr
     type(c_ptr) :: data_to_send_c_ptr = c_null_ptr
   end type thread_queue_element
 
 
-  ! What gets passed into the thread.
+  !* What gets passed into the thread.
   type :: thread_argument
     logical(c_bool), pointer :: active_flag => null()
     type(c_ptr) :: data = c_null_ptr
     type(c_ptr) :: mutex_ptr = c_null_ptr
   end type thread_argument
 
-
-!* for_p_thread.
 
   interface
 
