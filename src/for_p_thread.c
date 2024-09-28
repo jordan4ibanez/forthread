66f6c2b52101e38b8a6f607fd71c43af15af9b0e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-28 10:09:30 -0400

    We're going to move an incredible hackjob into C

--- src/for_p_thread.c
+++ src/for_p_thread.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <assert.h>
 // This one is for looking up error IDs.
 // #include <errno.h>
 // Go through to <errno-base.h>
@@ -8,15 +9,21 @@
 // You can ctrl - click this to get there.
 // int test = ENOENT;
 
-const static int pthread_mutex_t_size = sizeof(pthread_mutex_t);
-
-const static int pthread_create_detached_id = PTHREAD_CREATE_DETACHED;
+//! TODO: Needs to be tested on FreeBSD.
 
 //* Make this portable.
 
-int for_p_thread_get_pthread_mutex_t_width()
+pthread_mutex_t *for_p_thread_create_mutex()
 {
-  return pthread_mutex_t_size;
+  pthread_mutex_t *mutex = malloc(sizeof(pthread_mutex_t));
+
+  // If this returns anything but 0, bail out.
+  if (pthread_mutex_init(mutex, NULL))
+  {
+    assert(false);
+  }
+
+  return mutex;
 }
 
 //* Getting the number of available threads.
