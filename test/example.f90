5afa17bee9c29fa0faeb031ef1eafad6f80b1a43
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-28 18:00:22 -0400

    re-word this

--- test/example.f90
+++ test/example.f90
@@ -156,8 +156,7 @@ program thread_example
 
     !* Don't have 1024 cpu cores? No problem! That's why we have RAM. 8)
     !! An extremely important note:
-    !* If you create too many threads in one shot, the Linux arena heap will start
-    !* to go insane and use a lot of memory!
+    !* If you create too many threads in one shot, the Linux arena heap will try to hold onto memory.
     do i = 1,1024
 
       !* Reallocate the pointer every loop.
