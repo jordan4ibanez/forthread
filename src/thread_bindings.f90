a03a7488cd707d1d7f53eb983290879ed3c4d183
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-01 08:47:29 -0400

    this is no longer used

--- src/thread_bindings.f90
+++ src/thread_bindings.f90
@@ -36,14 +36,6 @@ module thread_bindings
     end function for_p_thread_get_cpu_threads
 
 
-    function for_p_thread_get_pthread_mutex_t_width() result(data_width) bind(c, name = "for_p_thread_get_pthread_mutex_t_width")
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      integer(c_int) :: data_width
-    end function for_p_thread_get_pthread_mutex_t_width
-
-
     !* BEGIN FUNCTION BLUEPRINTS.
 
 
