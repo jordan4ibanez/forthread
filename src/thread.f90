b9ca6f471c89682b95a04ac108920cff1d52315b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-02 02:48:04 -0400

    Soup this up

--- src/thread.f90
+++ src/thread.f90
@@ -111,16 +111,21 @@ contains
 
 
   !* Create a new joinable thread.
-  !* Returns you the thread struct.
-  function create_joinable(subroutine_c_funptr, argument_ptr) result(tid) bind(c)
+  function thread_create_joinable(subroutine_c_funptr, argument_ptr) result(tid) bind(c)
+    use :: internal_temp_string
     implicit none
 
     type(c_funptr), intent(in), value :: subroutine_c_funptr
     type(c_ptr), intent(in), value :: argument_ptr
     integer(c_int64_t) :: tid
+    integer(c_int) :: status
 
-    tid = internal_for_p_thread_create_thread(subroutine_c_funptr, argument_ptr, logical(.false., c_bool))
-  end function create_joinable
+    status = pthread_create(tid, subroutine_c_funptr, argument_ptr)
+
+    if (status /= 0) then
+      error stop "[Thread] Error: Failed to create a thread. Error status: ["//int_to_string(status)//"]"
+    end if
+  end function thread_create_joinable
 
 
   !* Join a thread back into the main thread.
