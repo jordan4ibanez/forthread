155ddf09ca139a45d4e47ad84abc30fb06255e75
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-02 08:53:15 -0400

    I missed this naming

--- src/thread_mutex.f90
+++ src/thread_mutex.f90
@@ -6,36 +6,36 @@ module thread_mutex
   interface
 
 
-    function thread_create_mutex() result(c_mutex_pointer) bind(c, name = "for_p_thread_create_mutex")
+    function thread_create_mutex() result(c_mutex) bind(c, name = "for_p_thread_create_mutex")
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr) :: c_mutex_pointer
+      type(c_ptr) :: c_mutex
     end function thread_create_mutex
 
 
-    subroutine thread_destroy_mutex(rwlock) bind(c, name = "for_p_thread_destroy_mutex")
+    subroutine thread_destroy_mutex(c_mutex) bind(c, name = "for_p_thread_destroy_mutex")
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr), intent(in), value :: rwlock
+      type(c_ptr), intent(in), value :: c_mutex
     end subroutine thread_destroy_mutex
 
 
-    function thread_lock_mutex(rwlock) result(status) bind(c, name = "pthread_mutex_lock")
+    function thread_lock_mutex(c_mutex) result(status) bind(c, name = "pthread_mutex_lock")
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr), intent(in), value :: rwlock
+      type(c_ptr), intent(in), value :: c_mutex
       integer(c_int) :: status
     end function thread_lock_mutex
 
 
-    function thread_unlock_mutex(rwlock) result(status) bind(c, name = "pthread_mutex_unlock")
+    function thread_unlock_mutex(c_mutex) result(status) bind(c, name = "pthread_mutex_unlock")
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr), intent(in), value :: rwlock
+      type(c_ptr), intent(in), value :: c_mutex
       integer(c_int) :: status
     end function thread_unlock_mutex
 
